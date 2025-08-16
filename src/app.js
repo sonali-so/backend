d69a0503cf70f7d8296e95bbed248576d3d0682a
sonali-so <sonalisethi162@gmail.com>
2025-08-16 11:13:50 +0530

    util code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 // src/app.js
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN ,
+    Credentials:true
+}));
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public")); // Serve static files from the "public" directory 
+app.use(cookieParser());
+
 
 // Basic route
 app.get("/", (req, res) => {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+ // can be done using promise and try catch
+ //const asyncHandler=()=>{}
+ //const asyncHandler=(func)=>()=>{}
+ //const asyncHandler=(func)=>async()=>{}
+    //using try catch
+    /*
+ const asyncHandler=(func)=>async(req,res,next)=>{
+    try{
+        await func(req,res,next)
+    }catch(error){
+        res.status(error.code||500).json({
+            success:false,
+            message:error.message
+        })
+    }
+ }*/
+
+    //using promise
+    const asyncHandler=(requestHandler)=>{
+        (res,req,next)=>{
+            Promise.resolve(requestHandler(req,res,next)).
+            catch((err)=>next(err))
+        }
+    }
+    export {asyncHandler}
+
