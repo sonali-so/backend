5c96dff952a909f7d8ad0b12d52880a29dc9ed3d
sonali-so <sonalisethi162@gmail.com>
2025-08-19 00:34:23 +0530

    add user and video model

---
+++ src/models/video.models.js
@@ -0,0 +1,55 @@
+import mongoose ,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"; 
+const videoSchema = new Schema({
+    watchHistory:
+   { 
+    type: String, 
+    required: true, 
+    unique:true,
+    lowercase:true,
+    trim:true,
+    index:true
+  },
+  videoFile:
+   { 
+    type: String, 
+    required: true
+  },
+  thumbnail:
+   { 
+    type: String, 
+    required: true, 
+  },
+  title:
+  { 
+    type: String, 
+    required: true, 
+  },
+  description:{
+    type: String,
+    required: true
+  },
+  duration:
+  { 
+    type: Number, 
+    required: true, 
+  },
+  views:
+  { 
+    type: Number, 
+    default:0 
+  },
+  isPublished:
+  { 
+    type: Boolean, 
+    default: true
+  },
+  owner:
+  {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+},{timestamps:true})
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video=mongoose.models("Video",videoSchema)
